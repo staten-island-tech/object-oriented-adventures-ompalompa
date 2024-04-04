a95d3ecb947e2d0c215a4680b0b1a96b8f33238e
AlvinnH <alvinh27@nycstudents.net>
2024-04-04 14:04:27 -0400

    asd

---
+++ data/characters.py
@@ -0,0 +1,1 @@
+Bots = int(input("How many bots do you want to play with?: "))
--- data/dices.py
+++ data/dices.py
@@ -1,7 +1,15 @@
 import random
-
-over = True
-if over == True:
+import characters
+for characters in range(characters):
     number_list = ["1","2","3","4","5","6"]
     random_number = random.choice(number_list)
-    print(random_number)
+    random_number2 = random.choice(number_list)
+    Dice_rolls = []
+    start = True
+    if start == True:
+        print(f"You have rolled a {random_number}!")
+        Dice_rolls.append(random_number)
+        print(f"You have rolled a {random_number2}!")
+        Dice_rolls.append(random_number2)
+    Total = sum(Dice_rolls)
+    print(Total)
