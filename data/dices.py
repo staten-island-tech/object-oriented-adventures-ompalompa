be57b74e29c13e2fa374c013d0152801b7c220fa
AlvinnH <alvinh27@nycstudents.net>
2024-04-08 14:07:02 -0400

    asd

--- data/dices.py
+++ data/dices.py
@@ -1,9 +1,11 @@
 import random
-import characters
-for characters in range(characters):
+User_Data = []
+x = int(input("How many players are playing? "))
+for x in range(x):
+    username = input("What is the name of this player? ")
     number_list = ["1","2","3","4","5","6"]
-    random_number = random.choice(number_list)
-    random_number2 = random.choice(number_list)
+    random_number = int(random.choice(number_list))
+    random_number2 = int(random.choice(number_list))
     Dice_rolls = []
     start = True
     if start == True:
@@ -11,5 +13,13 @@ for characters in range(characters):
         Dice_rolls.append(random_number)
         print(f"You have rolled a {random_number2}!")
         Dice_rolls.append(random_number2)
-    Total = sum(Dice_rolls)
-    print(Total)
+        TotalDice = sum(Dice_rolls)
+        player = {
+        'username': str(username),
+        'dices': TotalDice,
+        }
+        User_Data.append(player)
+    
+for player in User_Data:
+    print("Username: ", player['username'])
+    print("Dice Total: ", player['dices'])
