1570fe9f5cf55b5960c1fc33df67f141cd3c597c
Jonathnan9 <158211587+Jonathnan9@users.noreply.github.com>
2024-05-23 13:59:49 -0400

    eeeee

--- data/tkinterboard.py
+++ data/tkinterboard.py
@@ -39,12 +39,18 @@ class TkinterBoard:
         self.canvas.tag_bind(text_id, "<Button-1>", lambda event: command())
 
     def name(self):
-        print("name")
         self.clear_screen()
         x = "Name"
         self.setup_screen(x)
-        self.canvas.create_text(self.root.winfo_screenwidth() / 2, self.root.winfo_screenheight() / 10, text="Type a Name", font=("Comic Sans MS", 80, "bold"), fill=None)
+        button_font = ("Comic Sans MS", 20, "bold")
+        self.canvas.create_text(self.root.winfo_screenwidth() / 2, self.root.winfo_screenheight() / 10, text="Enter A Name", font=("Comic Sans MS", 80, "bold"), fill="white")
 
+        self.name = tk.Entry(self.canvas, font=("Comic Sans MS", 20), fill=None)
+        self.name.place(relx=0.5,rely=0.3, anchor=tk.CENTER)
+
+        self.makebutton("Enter", button_font, self.root.winfo_screenwidth() / 2, self.root.winfo_screenheight() / 2 - 80, command=self.token)
+        name= self.name.get()
+        print(name)
 
     def token(self):
         self.clear_screen()
@@ -65,7 +71,7 @@ class TkinterBoard:
         self.setup_screen(x)
         self.canvas.create_text(self.root.winfo_screenwidth() / 2, self.root.winfo_screenheight() / 10, text="Rules", font=("Comic Sans MS", 80, "bold"), fill=None)
 
-        frame = tk.Frame(self.canvas, background="systemTransparent")
+        frame = tk.Frame(self.canvas, fill=None)
         frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER, width=self.root.winfo_screenwidth() * 0.8, height=self.root.winfo_screenheight() * 0.6)
         
         scrollbar = tk.Scrollbar(frame)
