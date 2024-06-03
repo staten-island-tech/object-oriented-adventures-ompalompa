742cfc4640534a8c18c6fc237a248d20e62e3b4b
Jonathnan9 <158211587+Jonathnan9@users.noreply.github.com>
2024-06-03 14:00:47 -0400

    ddd

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 from data.tkinterboard import TkinterBoard
+from tkinter import *
+import tkinter as tk
 
-TkinterBoard.self.mainscreen()
+
+root = tk.Tk()
+app = TkinterBoard(root, "data/images/b1.png")
+app.mainscreen()
